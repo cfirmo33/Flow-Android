a646edaf75c061654d5841a7d2a599d474261fc8
Yuya Kaido <yuyakaido@gmail.com>
2016-07-30 19:08:21 +0900

    Add test

---
+++ app/src/test/java/com.yuyakaido.android.flow/SampleTest.kt
@@ -0,0 +1,16 @@
+package com.yuyakaido.android.flow
+
+import com.taroid.knit.should
+import org.junit.Test
+
+/**
+ * Created by yuyakaido on 7/30/16.
+ */
+class SampleTest {
+
+    @Test
+    fun sample() {
+        (1 + 1).should be 2
+    }
+
+}
