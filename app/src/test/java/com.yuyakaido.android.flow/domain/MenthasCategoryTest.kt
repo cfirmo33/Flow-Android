34e2a255887c3345708f93501f5d385e1ed181a5
Yuya Kaido <yuyakaido@gmail.com>
2016-07-31 03:07:26 +0900

    Add MenthasCategoryTest

---
+++ app/src/test/java/com.yuyakaido.android.flow/domain/MenthasCategoryTest.kt
@@ -0,0 +1,24 @@
+package com.yuyakaido.android.flow.domain
+
+import com.taroid.knit.should
+import com.yuyakaido.android.flow.domain.entity.MenthasCategory
+import com.yuyakaido.android.flow.misc.FlowTest
+import org.junit.Test
+
+/**
+ * Created by yuyakaido on 7/31/16.
+ */
+class MenthasCategoryTest : FlowTest() {
+
+    @Test
+    fun equalsTest() {
+        val java = MenthasCategory("java")
+        val kotlin = MenthasCategory("kotlin")
+        val altJava = MenthasCategory("java")
+
+        java.equals(java).should be true
+        java.equals(kotlin).should be false
+        java.equals(altJava).should be true
+    }
+
+}
