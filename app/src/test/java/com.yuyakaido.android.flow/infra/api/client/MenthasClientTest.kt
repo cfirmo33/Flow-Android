f98d840ce0d76cb46dfe26a31168e7c1f6bbc7ae
Yuya Kaido <yuyakaido@gmail.com>
2016-08-18 22:25:44 +0900

    Fix MenthasClientTest

--- app/src/test/java/com.yuyakaido.android.flow/infra/api/client/MenthasClientTest.kt
+++ app/src/test/java/com.yuyakaido.android.flow/infra/api/client/MenthasClientTest.kt
@@ -1,7 +1,9 @@
 package com.yuyakaido.android.flow.infra.api.client
 
 import com.taroid.knit.should
+import com.yuyakaido.android.flow.domain.entity.Article
 import com.yuyakaido.android.flow.domain.entity.Category
+import com.yuyakaido.android.flow.domain.entity.MenthasCategory
 import com.yuyakaido.android.flow.infra.api.common.ApiClientGenerator
 import com.yuyakaido.android.flow.misc.FlowTest
 import com.yuyakaido.android.flow.util.ResponseUtil
@@ -34,6 +36,25 @@ class MenthasClientTest : FlowTest() {
         subscriber.onNextEvents[0].size.should be 17
 
         server.shutdown()
+    }
+
+    @Test
+    fun getArticlesTest() {
+        val file = File("src/test/assets/response/menthas_article.json")
+        val server = MockWebServer()
+        server.enqueue(ResponseUtil.createMockResponse(file))
+        server.start()
+
+        val api = ApiClientGenerator.createJsonClient(MenthasApi::class.java, server.url("").toString())
+        val client = MenthasClient(api)
+
+        val subscriber = TestSubscriber<List<Article>>()
+        client.getArticles(MenthasCategory("", ""), 0).subscribe(subscriber)
+
+        subscriber.assertNoErrors()
+        subscriber.onNextEvents.size.should be 1
+        subscriber.assertCompleted()
+        subscriber.onNextEvents[0].size.should be 33
     }
 
 }
