676928529354a1352100486ae0d3cfd91101233a
Yuya Kaido <yuyakaido@gmail.com>
2016-08-20 23:41:23 +0900

    Fix GetCategoryUseCaseTest

--- app/src/test/java/com.yuyakaido.android.flow/domain/usecase/GetCategoryUseCaseTest.kt
+++ app/src/test/java/com.yuyakaido.android.flow/domain/usecase/GetCategoryUseCaseTest.kt
@@ -4,6 +4,8 @@ import com.sys1yagi.kmockito.invoked
 import com.sys1yagi.kmockito.mock
 import com.taroid.knit.should
 import com.yuyakaido.android.flow.domain.entity.Category
+import com.yuyakaido.android.flow.domain.entity.HatenaCategory
+import com.yuyakaido.android.flow.domain.entity.MenthasCategory
 import com.yuyakaido.android.flow.domain.entity.Site
 import com.yuyakaido.android.flow.infra.repository.HatenaRepository
 import com.yuyakaido.android.flow.infra.repository.MenthasRepository
@@ -18,14 +20,15 @@ import rx.observers.TestSubscriber
 class GetCategoryUseCaseTest : FlowTest() {
 
     @Test
-    fun getMenthasCategoriesTest() {
+    fun getEmptyMenthasCategoriesTest() {
         val menthasRepository = mock<MenthasRepository>()
         val hatenaRepository = mock<HatenaRepository>()
         menthasRepository.getCategories().invoked.thenReturn(Single.just(arrayListOf()))
-        val useCase = GetCategoryUseCase(menthasRepository, hatenaRepository)
 
         val subscriber = TestSubscriber<List<Category>>()
-        useCase.getCategories(Site.Menthas).subscribe(subscriber)
+        GetCategoryUseCase(menthasRepository, hatenaRepository)
+                .getCategories(Site.Menthas)
+                .subscribe(subscriber)
 
         subscriber.assertNoErrors()
         subscriber.onNextEvents.size.should be 1
@@ -33,10 +36,38 @@ class GetCategoryUseCaseTest : FlowTest() {
     }
 
     @Test
-    fun getHatenaHotCategoriesTest() {
+    fun getMenthasCategoriesTest() {
+        val size = 10
+        val categories = Array(size) { it.toString() }
+                .map { MenthasCategory(it, it) }
+                .toList()
+
         val menthasRepository = mock<MenthasRepository>()
         val hatenaRepository = mock<HatenaRepository>()
-        hatenaRepository.getCategories().invoked.thenReturn(Single.just(arrayListOf()))
+        menthasRepository.getCategories().invoked.thenReturn(Single.just(categories))
+
+        val subscriber = TestSubscriber<List<Category>>()
+        GetCategoryUseCase(menthasRepository, hatenaRepository)
+                .getCategories(Site.Menthas)
+                .subscribe(subscriber)
+
+        subscriber.assertNoErrors()
+        subscriber.onNextEvents.size.should be 1
+        subscriber.onNextEvents[0].size.should be size
+        subscriber.onNextEvents[0].forEachIndexed { i, actual ->
+            val expect = categories[i]
+
+            actual.name().should be expect.name
+            actual.api().should be expect.api
+        }
+        subscriber.assertCompleted()
+    }
+
+    @Test
+    fun getEmptyHatenaHotCategoriesTest() {
+        val menthasRepository = mock<MenthasRepository>()
+        val hatenaRepository = mock<HatenaRepository>()
+        hatenaRepository.getCategories().invoked.thenReturn(Single.just(listOf()))
         val useCase = GetCategoryUseCase(menthasRepository, hatenaRepository)
 
         val subscriber = TestSubscriber<List<Category>>()
@@ -48,10 +79,38 @@ class GetCategoryUseCaseTest : FlowTest() {
     }
 
     @Test
-    fun getHatenaNewCategoriesTest() {
+    fun getHatenaHotCategoriesTest() {
+        val size = 10
+        val categories = Array(size) { it.toString() }
+                .map { HatenaCategory(it, it) }
+                .toList()
+
         val menthasRepository = mock<MenthasRepository>()
         val hatenaRepository = mock<HatenaRepository>()
-        hatenaRepository.getCategories().invoked.thenReturn(Single.just(arrayListOf()))
+        hatenaRepository.getCategories().invoked.thenReturn(Single.just(categories))
+
+        val subscriber = TestSubscriber<List<Category>>()
+        GetCategoryUseCase(menthasRepository, hatenaRepository)
+                .getCategories(Site.HatenaHot)
+                .subscribe(subscriber)
+
+        subscriber.assertNoErrors()
+        subscriber.onNextEvents.size.should be 1
+        subscriber.onNextEvents[0].size.should be size
+        subscriber.onNextEvents[0].forEachIndexed { i, actual ->
+            val expect = categories[i]
+
+            actual.name().should be expect.name
+            actual.api().should be expect.api
+        }
+        subscriber.assertCompleted()
+    }
+
+    @Test
+    fun getEmptyHatenaNewCategoriesTest() {
+        val menthasRepository = mock<MenthasRepository>()
+        val hatenaRepository = mock<HatenaRepository>()
+        hatenaRepository.getCategories().invoked.thenReturn(Single.just(listOf()))
         val useCase = GetCategoryUseCase(menthasRepository, hatenaRepository)
 
         val subscriber = TestSubscriber<List<Category>>()
@@ -59,6 +118,34 @@ class GetCategoryUseCaseTest : FlowTest() {
 
         subscriber.assertNoErrors()
         subscriber.onNextEvents.size.should be 1
+        subscriber.assertCompleted()
+    }
+
+    @Test
+    fun getHatenaNewCategoriesTest() {
+        val size = 10
+        val categories = Array(size) { it.toString() }
+                .map { HatenaCategory(it, it) }
+                .toList()
+
+        val menthasRepository = mock<MenthasRepository>()
+        val hatenaRepository = mock<HatenaRepository>()
+        hatenaRepository.getCategories().invoked.thenReturn(Single.just(categories))
+
+        val subscriber = TestSubscriber<List<Category>>()
+        GetCategoryUseCase(menthasRepository, hatenaRepository)
+                .getCategories(Site.HatenaNew)
+                .subscribe(subscriber)
+
+        subscriber.assertNoErrors()
+        subscriber.onNextEvents.size.should be 1
+        subscriber.onNextEvents[0].size.should be size
+        subscriber.onNextEvents[0].forEachIndexed { i, actual ->
+            val expect = categories[i]
+
+            actual.name().should be expect.name
+            actual.api().should be expect.api
+        }
         subscriber.assertCompleted()
     }
 
